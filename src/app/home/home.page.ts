be7171ba2cb2a4ee567fc091f2018a53ad130e01
Stephen Ray Guedea <sguedea@appddictionstudio.com>
2021-06-24 14:23:33 -0500

    started todo app / data displays in cards / included btns for sharing - deleting / add item btn

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  todoList = [
+    {
+    itemName: 'Coding',
+    itemDueDate: '7/13/2021',
+    itemPriority: 'high',
+    itemCategory: 'Work'
+    },
+    {
+      itemName: 'Design',
+      itemDueDate: '7/18/2021',
+      itemPriority: 'low',
+      itemCategory: 'Work'
+    },
+    {
+      itemName: 'Shop for gift',
+      itemDueDate: '7/31/2021',
+      itemPriority: 'middle',
+      itemCategory: 'Personnel'
+    },
+    {
+      itemName: 'Record DJ mix',
+      itemDueDate: '7/31/2021',
+      itemPriority: 'high',
+      itemCategory: 'Personnel'
+    }
+    
+];
+
+today : number = Date.now();
+
   constructor() {}
 
 }
